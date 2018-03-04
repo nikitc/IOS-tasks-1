a3aa2ae591c1b03dbd4d76613a9f1c22daab3cf9
Admin <admin@Mac-Admin.local>
2018-03-04 18:49:42 +0300

    add project

---
+++ Homework1.playground/Sources/FileNoteBook.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+class FileNotebook {
+    private(set) var notes = [Note]()
+    
+    func addNote(note: Note) {
+        //TODO
+    }
+    
+    func deleteNote(uid: String) {
+        //TODO
+    }
+    
+    func saveAllNotes(filename: String) {
+        //TODO
+    }
+    
+    func loadNotes(filename: String) {
+        //TODO
+    }
+}
---
+++ Homework1.playground/Sources/Importance.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum Importance {
+    case Important
+    case Normal
+    case Unimportant
+}
